f06c8a02bd82ec49bd421a2cc8866ed1d5e32b1c
Lukas Soudek <lukas.soudek@gmail.com>
2025-01-24 15:49:40 +0100

    logovani do updateSelectionFromCoords

--- src/utils/selection.js
+++ src/utils/selection.js
@@ -158,6 +158,7 @@ export const removeCopyingSelection = function() {
 export const updateSelectionFromCoords = function(x1, y1, x2, y2, origin) {
     const obj = this;
 
+    console.log('updateSelectionFromCoords', x1, y1, x2, y2, origin);
     // select column
     if (y1 == null) {
         y1 = 0;
