be01c05745c21dea1b4049f71601834d37fad7ac
Lukas Soudek <soudekl@globtech.cz>
2025-10-10 16:04:32 +0200

    up/down

--- dist/index.js
+++ dist/index.js
@@ -2419,13 +2419,13 @@ const updateSelectionFromCoords = function(x1, y1, x2, y2, origin) {
        // if (endRowIndex < startRowIndex) {
             console.log('obj.preventOnSelection 2');
             // doslo ke scrollu
-            if (obj.scrollDirection == "up") {
+            // if (obj.scrollDirection == "up") {
                 console.log('obj.preventOnSelection 3');
                 if (obj.startSelRow < endRowIndex) {
                     console.log('TODO GO UP -> SET Y1 x1 = ', x1, ', y1 = ' , y1 , ', x2 = ', x2, ' y2 = ', y2, ', obj.startSelRow = ', obj.startSelRow, ', obj.endSelRow = ', obj.endSelRow);
                     obj.selectedCell = [x1, y2, x2, y2];
                 }
-            }
+            // }
         //}
         obj.preventOnSelection = false;
     } 
