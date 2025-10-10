6647451c2ef585f8630136ec579440f6af69f543
Lukas Soudek <soudekl@globtech.cz>
2025-10-10 10:41:28 +0200

    Scroll up

--- src/utils/selection.js
+++ src/utils/selection.js
@@ -157,9 +157,7 @@ export const removeCopyingSelection = function() {
 }
 
 export const updateSelectionFromCoords = function(x1, y1, x2, y2, origin) {
-    const obj = this;
-
-    console.clear();    
+    const obj = this;    
     console.log('onclick updateSelectionFromCoords x1, y1, x2, y2,', x1, y1, x2, y2);
 
     var selectWholeColumn = false;
