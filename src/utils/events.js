df1d071c5ef651868fa9f3d01bee001f7bb01597
Lukas Soudek <soudekl@globtech.cz>
2025-10-13 14:05:01 +0200

    onPrevent

--- src/utils/events.js
+++ src/utils/events.js
@@ -697,10 +697,7 @@ const mouseOverControls = function (e) {
                                         const rowToId = rowId ? libraryBase.jspreadsheet.current.getRowData(rowId)[0] : undefined;
                                         const cell1ToId = cell1 ? libraryBase.jspreadsheet.current.getRowData(cell1)[0] : undefined;
                                         const cell2ToId = cell2 ? libraryBase.jspreadsheet.current.getRowData(cell2)[0] : undefined;
-
-                                        // console.log('--mouseOverControls-- indexes row = ', rowId, 'y1 = ', cell1, ', y2 = ', cell2, ', preventOnSelection = ', preventOnSelection);
-                                        // console.log('--mouseOverControls-- rowToId = ', rowToId, 'y1Id = ', cell1ToId, ' y2Id = ', cell2ToId);
-
+                                       
                                         if (!cell2) {
                                             // console.log('--0. mouseOverControls-- vybrana pouze jedna bunka');
                                             libraryBase.jspreadsheet.current.mouseOverDirection = 'none';
@@ -727,8 +724,12 @@ const mouseOverControls = function (e) {
                                             // updateSelectionFromCoords.call(libraryBase.jspreadsheet.current, columnId, rowId, libraryBase.jspreadsheet.current.selectedCell[2], libraryBase.jspreadsheet.current.selectedCell[3], e);
                                         }
 
-                                        if (!libraryBase.jspreadsheet.current.preventOnSelection)
-                                            updateSelectionFromCoords.call(libraryBase.jspreadsheet.current, libraryBase.jspreadsheet.current.selectedCell[0], libraryBase.jspreadsheet.current.selectedCell[1], columnId, rowId, e);
+                                        console.log('--mouseOverControls-- indexes row = ', rowId, 'y1 = ', cell1, ', y2 = ', cell2, ', preventOnSelection = ', preventOnSelection, ', mouseOverDirection = ', libraryBase.jspreadsheet.current.mouseOverDirection);
+                                        // console.log('--mouseOverControls-- rowToId = ', rowToId, 'y1Id = ', cell1ToId, ' y2Id = ', cell2ToId);
+
+
+                                        // if (!libraryBase.jspreadsheet.current.preventOnSelection)
+                                        updateSelectionFromCoords.call(libraryBase.jspreadsheet.current, libraryBase.jspreadsheet.current.selectedCell[0], libraryBase.jspreadsheet.current.selectedCell[1], columnId, rowId, e);
                                         // else {
                                         //     if (libraryBase.jspreadsheet.current.mouseOverDirection == "up" || libraryBase.jspreadsheet.current.mouseOverDirection == "sellDownAndThanUp")
                                         //     {
