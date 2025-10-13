40f3f6253afef77eba00d07fda6318791d0b9f99
Lukas Soudek <soudekl@globtech.cz>
2025-10-13 19:04:30 +0200

    keyboard move

--- src/utils/selection.js
+++ src/utils/selection.js
@@ -421,7 +421,7 @@ export const updateSelectionFromCoords = function(x1, y1, x2, y2, origin) {
     // pohyb na klavesnici
     else {
         console.log('keyboard input obj.keyDirection = ', obj.keyDirection);
-        if (obj.keyDirection != -1) {
+        // if (obj.keyDirection != -1) {
         // if (!obj.preventOnSelection) {
             obj.startSelCol = x1;
             obj.endSelCol = x2;     
@@ -453,7 +453,7 @@ export const updateSelectionFromCoords = function(x1, y1, x2, y2, origin) {
             }
 
             obj.keyDirection = -1;
-        }
+        // }
         // }
     }
 
